f44f2cb6af6b059426a33b726532ffe2c33e5658
Santiago Palmet Salcedo <santi1226@gmail.com>
2023-02-08 22:49:18 -0500

    Que el santiago del mañana se encargue de ver que carajo hice

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get_rx/src/rx_typedefs/rx_typedefs.dart';
 
 import '../../utils/color_utils.dart';
 import '../widgets/color_palette.dart';
@@ -25,10 +26,32 @@ class ColorSelectionPage extends StatelessWidget {
           ],
         ),
         // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
-        )));
+        body: (Center(
+            child: SingleChildScrollView(
+          child: Column(children: [
+            ColorPalette(
+              baseColor: 'FFD4D4',
+              baseColor2: 'FFFFE8',
+              baseColor3: 'CDE990',
+              baseColor4: 'AACB73',
+              callback: showColor,
+            ),
+            ColorPalette(
+              baseColor: '7286D3',
+              baseColor2: '8EA7E9',
+              baseColor3: 'E5E0FF',
+              baseColor4: 'FFF2F2',
+              callback: showColor,
+            ),
+            ColorPalette(
+              baseColor: 'B9F3FC',
+              baseColor2: 'AEE2FF',
+              baseColor3: '93C6E7',
+              baseColor4: 'FEDEFF',
+              callback: showColor,
+            ),
+          ]),
+        ))));
   }
 
   // está es la función que será llamada con cada click a un ColorPalette
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -4,25 +4,31 @@ import '../../utils/color_utils.dart';
 class ColorPalette extends StatelessWidget {
   const ColorPalette({
     Key? key,
-    // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
+    required this.baseColor,
+    required this.baseColor2,
+    required this.baseColor3,
+    required this.baseColor4,
+    required this.callback,
   }) : super(key: key);
 
-  // atributos de la clase, por ejemplo
-  //final String baseColor;
-
-  // también se puede tener como un atributo un callback, necesiario ya que es un
-  // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final String baseColor;
+  final String baseColor2;
+  final String baseColor3;
+  final String baseColor4;
+  final Function(String) callback;
 
   @override
   Widget build(BuildContext context) {
     // padding para crear espacio alrededor del widget
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
-    );
+        padding: const EdgeInsets.all(8.0),
+        // vamos a mostrar los elementos en un row
+        child: Row(children: [
+          Expanded(
+              flex: 4,
+              child: GestureDetector(
+                  child: Container(
+                      height: 100, color: ColorUtils.FromHex(baseColor))))
+        ]));
   }
 }
